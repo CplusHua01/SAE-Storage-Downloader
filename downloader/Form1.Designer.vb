1a2cf634a832ba937c599450b99f1c3b2a150ed9
CplusHua <gonghua21@gmail.com>
2012-05-17 17:39:52 +0800

    SAE-Storage-Downloader

---
+++ downloader/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.URLbox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PHPURLbox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.StoreApp = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'URLbox
+        '
+        Me.URLbox.Location = New System.Drawing.Point(179, 112)
+        Me.URLbox.Name = "URLbox"
+        Me.URLbox.Size = New System.Drawing.Size(403, 21)
+        Me.URLbox.TabIndex = 1
+        Me.URLbox.Text = "http://"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(50, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Storage地址："
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(179, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(281, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "例如:http://storeapp-storeapp.stor.sinaapp.com"
+        '
+        'PHPURLbox
+        '
+        Me.PHPURLbox.Location = New System.Drawing.Point(179, 196)
+        Me.PHPURLbox.Name = "PHPURLbox"
+        Me.PHPURLbox.Size = New System.Drawing.Size(403, 21)
+        Me.PHPURLbox.TabIndex = 1
+        Me.PHPURLbox.Text = "http://"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.Location = New System.Drawing.Point(50, 196)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(112, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "PHP文件地址："
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(179, 224)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(251, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "例如:http://storeapp.sinaapp.com/down.php"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = CType(resources.GetObject("PictureBox1.ErrorImage"), System.Drawing.Image)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(497, 505)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(151, 37)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(497, 439)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(117, 53)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "开始下载"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'StoreApp
+        '
+        Me.StoreApp.Location = New System.Drawing.Point(179, 289)
+        Me.StoreApp.Name = "StoreApp"
+        Me.StoreApp.Size = New System.Drawing.Size(403, 21)
+        Me.StoreApp.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label5.Location = New System.Drawing.Point(50, 289)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 16)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Storage名称"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(179, 317)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 12)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "例如:storeapp"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(95, 514)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(324, 23)
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("宋体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 523)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 14)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "下载状态："
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(649, 549)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.StoreApp)
+        Me.Controls.Add(Me.PHPURLbox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.URLbox)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "SAE-Storage文件下载器"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents URLbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PHPURLbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents StoreApp As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ downloader/Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+Public Class Form1
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Try
+            Dim PHPURL As String '从此地址获取文件列表
+            Dim DownURL As String 'Storage根目录
+            DownURL = URLbox.Text.ToString
+            PHPURL = PHPURLbox.Text.ToString + "?storeapp=" + StoreApp.Text.ToString
+            ' Initialize the WebRequest.
+            Dim myRequest As WebRequest = WebRequest.Create(PHPURL)
+            Dim myResponse As WebResponse = myRequest.GetResponse()
+            Dim reader As StreamReader = New StreamReader(myResponse.GetResponseStream)
+            Dim FileName As String
+            FileName = reader.ReadToEnd
+            Dim DownFileName() As String
+            DownFileName = FileName.Split("|")
+            Dim myWebClient As New WebClient
+            Dim i As Integer
+            Try
+                For i = 0 To DownFileName.Length - 3
+                    Dim temp As String() = New String() {}
+                    temp = DownFileName(i).Split("/")
+                    checkdirectory(temp)
+                    myWebClient.DownloadFile(DownURL + "/" + DownFileName(i), DownFileName(i))
+                    ProgressBar1.Value = (i / (DownFileName.Length - 3)) * 100
+                Next
+            Catch ex As Exception
+                MessageBox.Show("请检查是否已经正确输入Storage地址和PHP地址")
+            End Try
+            If (DownFileName(DownFileName.Length - 2) = "FILE DOWN OVER") Then
+                MessageBox.Show("总文件个数为:" + DownFileName(DownFileName.Length - 1) + vbCrLf + "下载已经完成!")
+            Else
+                MessageBox.Show("发生未知错误,部分文件可能下载失败")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("发生未知错误，请检查您的网络和输入状态是否正常后重试！")
+        End Try
+      
+
+
+    End Sub
+    Private Sub checkdirectory(temp As String())
+        Try
+            Dim path As String
+            path = temp(0)
+            If temp.Length = 1 Then
+                Return
+            End If
+            Dim i As Integer
+            i = 0
+            For i = 1 To temp.Length - 1
+                If (i < temp.Length - 1) And temp.Length > 1 Then
+                    path = path + "/" + temp(i)
+                End If
+            Next
+            
+            MessageBox.Show(path)
+            Try
+                If (Directory.Exists(path) = False) Then
+                    Directory.CreateDirectory(path)
+                End If
+            Catch ex As Exception
+                MessageBox.Show("创建文件夹时发生了一些错误，请手动创建与Storage相同的目录结构重试")
+            End Try
+        Catch ex As Exception
+            MessageBox.Show("Error")
+        End Try
+        
+
+    End Sub
+
+End Class
